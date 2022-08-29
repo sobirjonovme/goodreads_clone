6d859b5a190f04ff7009dda3948438fa7a62da60
sobirjonov_me <huntsman6004@gmail.com>
2022-08-29 11:04:39 +0500

    Added fieldset to CustomUserAdmin

--- users/admin.py
+++ users/admin.py
@@ -22,6 +22,24 @@ class CustomUserAdmin(UserAdmin):
         ),
     )
 
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (("Personal info"), {"fields": ("first_name", "last_name", "email", "profile_picture")}),
+        (
+            ("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+
 
 # Register your models here.
 admin.site.register(CustomUser, CustomUserAdmin)
